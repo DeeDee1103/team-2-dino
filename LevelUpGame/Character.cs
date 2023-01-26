a4ba85363ed5fc0f33f0b14f69a092074a3eed14
Laurnell <dddevacct16783@gmail.com>
2023-01-26 21:00:43 +0000

    Added Character Class

---
+++ LevelUpGame/Character.cs
@@ -0,0 +1,9 @@
+namespace levelup
+{
+     public class Character{
+          public static string DEFAULT_NAME { get; set; }
+          public string NAME { get; set; }
+          
+     }
+  
+    }

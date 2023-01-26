a7c39e1a26d3c39b837e43ca1c6e6d44952e0230
XueYo <snow4eternity@gmail.com>
2023-01-26 16:14:39 +0000

    let's try to compile2

--- LevelUpGame.Tests/Steps/MoveSteps.cs
+++ LevelUpGame.Tests/Steps/MoveSteps.cs
@@ -9,38 +9,39 @@ namespace DotNetExample.Tests.Steps
     [Binding]
     public class MoveSteps
     {
-        GameController testObj = new GameController();
-        int startX, startY, endX, endY;
-        GameController.DIRECTION direction;
-        Point currentPosition;
+        private GameController testObj = new GameController();
+        private int startX, startY, endX, endY;
+        private GameController.DIRECTION direction;
+        private Point currentPosition;
+
+        [Given(@"the character starts at position with XCoordinates(.*)")]
+        public void givenTheCharacterStartsAtX(int startX)
+        {
+          this.startX = startX;
+        }
+
+        [Given(@"starts at YCoordinates (.*)")]
+        public void givenTheCharaterStartsAtY(int startY)
+        {
+          this.startY = startY;
+        }
+
+        [Given(@"the player chooses to move in (.*)")]
+        public. void givenPlayerChoosesDirection(String direction)
+        {
+          this.direction = (GameController.DIRECTION)
+          Enum.Parse(typeof(GameController.DIRECTION), direction);
+        }
+  
+        [When(@"the character moves")]
+        public void whenTheCharacterMoves()
+        {
+          testObj.SetCharacterPosition(new Point(this.startX, this.startY));
+          testObj.Move(this.direction);
+          GameController.GameStatus status=testObj.GetStatus();
+          this.currentPosition=status.currentPosition=status.currentPosition;
+        }
     }
 
-    [Given(@"the character starts at position with XCoordinates(.*)")]
-    public void givenTheCharacterStartsAtX(int startX)
-    {
-      this.startX = null;
-    }
-
-  [Given(@"starts at YCoordinates (.*)")]
-  public void givenTheCharaterStartsAtY(int startY)
-  {
-    this.startY = startY;
-  }
-
-  [Given(@"the player chooses to move in (.*)")]
-  public. void givenPlayerChoosesDirection(String direction)
-  {
-    this.direction = (GameController.DIRECTION)
-    Enum.Parse(typeof(GameController.DIRECTION), direction);
-  }
- 
- [When(@"the character moves")]
- public void whenTheCharacterMoves()
- {
-  testObj.SetCharacterPosition(new Point(this.startX, this.startY));
-  testObj.Move(this.direction);
-  GameController.GameStatus status=testObj.GetStatus();
-  this.currentPosition=status.currentPosition=status.currentPosition;
- }
-
+  
 }

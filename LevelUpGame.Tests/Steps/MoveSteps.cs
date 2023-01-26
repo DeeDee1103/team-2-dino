6c70f0ecf1794c2f9d56eb1592dda9317563ab1e
XueYo <snow4eternity@gmail.com>
2023-01-26 15:52:51 +0000

    update movesteps.cs

--- LevelUpGame.Tests/Steps/MoveSteps.cs
+++ LevelUpGame.Tests/Steps/MoveSteps.cs
@@ -2,24 +2,30 @@ using System;
 using TechTalk.SpecFlow;
 using NUnit.Framework;
 using FluentAssertions;
+using levelup;
 
 namespace DotNetExample.Tests.Steps
 {
+    [Binding]
+    public class MoveSteps
+    {
+        GameController testObj = new GameController();
+        int startX, startY, endX, endY;
+        GameController.DIRECTION direction;
+        Point currentPosition;
+    }
 
-[Binding]
- public class MoveSteps
-{
-    GameController testObj = new GameController();
-    int startX, startY, endX, endY;
-    GameController.DIRECTION direction;
-    Point currentPosition;
-}
+    [Given(@"the character starts at position with XCoordinates(.*)")]
+    public void givenTheCharacterStartsAtX(int startX)
+    {
+      this.startX = null;
+    }
 
-[Given(@"the character starts at position with XCoordinates(.*)")]
-public void givenTheCharacterStartsAtX(int startX)
-{
-  this.startX = null;
-}
+  [Given(@"starts at YCoordinates (.*)")]
+  public void givenTheCharaterStartsAtY(int startY)
+  {
+    this.startY = startY;
+  }
 
-  
+  [Given(@"the ")]
 }

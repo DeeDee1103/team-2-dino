928f2586c1f6daa46458e9f1ccf145082fd88e06
XueYo <snow4eternity@gmail.com>
2023-01-27 15:57:30 +0000

    added test for position

--- LevelUpGame.Tests/levelup/PositionTest.cs
+++ LevelUpGame.Tests/levelup/PositionTest.cs
@@ -1,12 +1,25 @@
 using NUnit.Framework;
 using levelup;
+using System.Drawing; 
 
 namespace levelup{
 
     [TestFixture]
     public class PositionTest
     {
+        private Position? testPosition;
 
+          [SetUp]
+        public void SetUp()
+        {
+            testPosition = new Position(1,1);
+        }
+
+        [Test]
+        public void IsPositionInitialized()
+        {
+            Assert.IsEqual(testPosition.coordinates, Point(1,1));
+        }
     }
     
 }

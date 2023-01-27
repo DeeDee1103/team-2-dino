18fc82f64758a3c0875debdc5d1455a5a6123e5f
XueYo <snow4eternity@gmail.com>
2023-01-27 16:00:04 +0000

    fix build

--- LevelUpGame/levelup/GameMap.cs
+++ LevelUpGame/levelup/GameMap.cs
@@ -5,7 +5,7 @@ namespace levelup
     public class GameMap
     {
         public Position[][] positions {get; set;}
-        public Position startingPosition = 0,0;
+        public Position startingPosition = new Position(0,0);
         public int numPositions = 100;
     }
 }
